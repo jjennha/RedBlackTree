a1a65fd20fb03d8cabcd65fdeac7741ce77a1b9f
jenny <jha@iastate.edu>
2019-11-25 23:32:58 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/jenny_ha_project2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jenny_ha_project2.dir/main.cpp.o"
+  "jenny_ha_project2"
+  "jenny_ha_project2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jenny_ha_project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
