402f55dc9fb45eb2f6b8501d8df010d01feca8c1
jenny <jha@iastate.edu>
2019-11-26 21:30:59 -0600

    makefile

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
-int main() {
+struct node {
+    int key;
+    node *parent;
+    char color;
+    node *left;
+    node *right;
+};
+class RedBlackTree {
+    node *root;
+    public:
+        RedBlackTree(){
+            root=NULL;
+        }
+        void insert(int v);
+        void rotateLeft(node*);
+        void rotateRight(node*);
+        void remove(int v);
+        void search(int v);
+
+};
+void RedBlackTree::insert(int v) {
+    node *n = new node();
+    n->key=v;
+
+}
+int main(int argc, char** argv) {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
