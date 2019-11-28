411f37b75c8e99a1997037f15f782388599b76f5
jenny <jha@iastate.edu>
2019-11-27 22:02:37 -0600

    tree initialization

--- main.cpp
+++ main.cpp
@@ -1,31 +1,134 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
-struct node {
+using namespace std;
+enum Color {
+    RED, BlACK
+};
+
+struct Node {
     int key;
-    node *parent;
-    char color;
-    node *left;
-    node *right;
+    Node *parent;
+    bool color;
+    Node *left;
+    Node *right;
+
+    Node(int k, bool color) {
+        this->key = k;
+        this->color = color;
+        left = right = parent = NULL;
+    }
+
+    Node(bool color) {
+        this->key = NULL;
+        this->color = color;
+        left = right = parent = NULL;
+    }
+
 };
+
 class RedBlackTree {
-    node *root;
-    public:
-        RedBlackTree(){
-            root=NULL;
-        }
-        void insert(int v);
-        void rotateLeft(node*);
-        void rotateRight(node*);
-        void remove(int v);
-        void search(int v);
+    Node *root;
+public:
+    RedBlackTree() {
+        root = NULL;
+    }
+
+    Node *initialize(vector<string> *nodeData);
+
+    void insert(int v);
+
+    void rotateLeft(Node *);
+
+    void rotateRight(Node *);
+
+    void remove(int v);
+
+    void search(int v);
 
 };
-void RedBlackTree::insert(int v) {
-    node *n = new node();
-    n->key=v;
 
+Node* strToNode(string data) {
+    Node *n;
+    char color = data.back();
+    if (color == 'b') {
+        color = BlACK;
+    } else if (color == 'r') {
+        color = RED;
+    }
+    if (color == 'f') {
+        color = BlACK;
+        n = new Node(color);
+    } else {
+        int val = stoi(data.substr(0, data.size() - 1));
+        n = new Node(val, color);
+    }
+    return n;
 }
-int main(int argc, char** argv) {
-    std::cout << "Hello, World!" << std::endl;
+
+Node *construct(vector<string> nodes, int start, int end) {
+    if (start > end) { return NULL; }
+    Node *node = strToNode(nodes[start]);
+
+    int i;
+    for(i=start; i<= end; i++){
+        cout << i << " - " << nodes.size() << endl;
+        if(strToNode(nodes[i])->key > node->key){
+            break;
+        }
+    }
+    node->left = construct(nodes,start+1,i-1);
+    node->right = construct(nodes,i,end);
+    return node;
+}
+
+Node *RedBlackTree::initialize(vector<string> *nodes) {
+    construct(*nodes, 0, nodes->size()-1);
+}
+
+void RedBlackTree::insert(int v) {
+}
+
+int main(int argc, char **argv) {
+    std::cout << argv[1] << std::endl;
+    vector<string> nodeInputs;
+    int searchThreads;
+    int modThreads;
+
+    if (argv[1]) {
+        string contents;
+        ifstream infile;
+        infile.open(argv[1]);
+        int line = 0;
+        while (!infile.eof()) {
+            getline(infile, contents);
+            stringstream ss(contents);
+
+            // get existing Node data
+            if (line == 0) {
+                string token;
+                while (getline(ss, token, ',')) {
+                    nodeInputs.push_back(token);
+                }
+            }
+            // get number of search threads
+            if (line == 2) {
+
+            }
+            // get number of modification threads
+            if (line == 3) {
+
+            }
+            // get operations data
+            if (line > 4) {}
+
+            line++;
+        }
+        infile.close();
+        RedBlackTree *rbtree = new RedBlackTree();
+        rbtree->initialize(&nodeInputs);
+    }
     return 0;
 }
